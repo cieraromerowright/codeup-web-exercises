5c598ca9744f89d8b6733705df39d836e3de6440
Ciera Romero-Wright <romerowrightciera@gmail.com>
2023-03-16 08:29:18 -0500

    completed 5 day forecast

--- js/weathermap.js
+++ js/weathermap.js
@@ -27,15 +27,58 @@ function geoCodeBuildWeather(searchString) {
 
             })
         $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${results[1]}&lon=${results[0]}&appid=${OPEN_WEATHER_APPID}&units=imperial`).done(function (data) {console.log(data)
-        let addHtml= ""
 
-          // 'day2'
-            addHtml += `<h4>Current location: ${data.city.name}</h4>`;
-            addHtml += `<h4>Current weather: ${data.list[4].weather[0].description}</h4>`;
-            addHtml += `<h5>Current average wind speed: ${parseInt(data.list[4].wind.speed)} knots</h5>`;
-            addHtml += `<h5>Current Temperature: ${parseInt(data.list[4].main.temp)} &deg;</h5>`;
-            addHtml += `<h5>Current humidity: ${parseInt(data.list[4].main.humidity)}</h5>`;
-            $("#weatherBody").html(addHtml);
+        let day1= ""
+
+          // day1
+            day1 += `<h4>Current location: ${data.city.name}</h4>`;
+            day1 += `<h4>Current weather: ${data.list[1].description}</h4>`;
+            day1 += `<h5>Current average wind speed: ${parseInt(data.list[1].wind.speed)} knots</h5>`;
+            day1 += `<h5>Current Temperature: ${parseInt(data.list[1].main.temp)} &deg;</h5>`;
+            day1 += `<h5>Current humidity: ${parseInt(data.list[1].main.humidity)}</h5>`;
+            $("#weatherBody").html(day1);
+
+
+            let day2= ""
+            // day2
+            day2 += `<h4>Current location: ${data.city.name}</h4>`;
+            day2 += `<h4>Current weather: ${data.list[4].description}</h4>`;
+            day2 += `<h5>Current average wind speed: ${parseInt(data.list[4].wind.speed)} knots</h5>`;
+            day2 += `<h5>Current Temperature: ${parseInt(data.list[4].main.temp)} &deg;</h5>`;
+            day2 += `<h5>Current humidity: ${parseInt(data.list[4].main.humidity)}</h5>`;
+            $("#weatherBody").html(day2)
+
+            let day3= ""
+            // day3
+            day3 += `<h4>Current location: ${data.city.name}</h4>`;
+            day3 += `<h4>Current weather: ${data.list[12].description}</h4>`;
+            day3 += `<h5>Current average wind speed: ${parseInt(data.list[12].wind.speed)} knots</h5>`;
+            day3 += `<h5>Current Temperature: ${parseInt(data.list[12].main.temp)} &deg;</h5>`;
+            day3 += `<h5>Current humidity: ${parseInt(data.list[12].main.humidity)}</h5>`;
+            $("#weatherBody").html(day3);
+
+            let day4=""
+
+            // day4
+            day4 += `<h4>Current location: ${data.city.name}</h4>`;
+            day4 += `<h4>Current weather: ${data.list[23].description}</h4>`;
+            day4 += `<h5>Current average wind speed: ${parseInt(data.list[23].wind.speed)} knots</h5>`;
+            day4 += `<h5>Current Temperature: ${parseInt(data.list[23].main.temp)} &deg;</h5>`;
+            day4 += `<h5>Current humidity: ${parseInt(data.list[23].main.humidity)}</h5>`;
+            $("#weatherBody").html(day4);
+
+            let day5=""
+            // day5
+            day5 += `<h4>Current location: ${data.city.name}</h4>`;
+            day5 += `<h4>Current weather: ${data.list[27].description}</h4>`;
+            day5 += `<h5>Current average wind speed: ${parseInt(data.list[27].wind.speed)} knots</h5>`;
+            day5 += `<h5>Current Temperature: ${parseInt(data.list[27].main.temp)} &deg;</h5>`;
+            day5 += `<h5>Current humidity: ${parseInt(data.list[27].main.humidity)}</h5>`;
+            $("#weatherBody").html(day5);
+
+
+
+
         })
         })
 };
@@ -45,9 +88,8 @@ function geoCodeBuildWeather(searchString) {
 
 
 
-
-$("#myBtn").click(function(e){
-    e.preventDefault();
-    geoCodeBuildWeather($("#searchInput").val());
+ $("#myBtn").click(function(e){
+   e.preventDefault();
+   geoCodeBuildWeather($("#searchInput").val());
 })
 
